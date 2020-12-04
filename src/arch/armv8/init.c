1b15f02828033f2536f2803bdb6af81f5a7a79be
Gero Schwäricke <gero.schwaericke@tum.de>
2020-12-04 19:05:39 +0100

    Update arch based on baremetal

--- src/arch/armv8/init.c
+++ src/arch/armv8/init.c
@@ -7,10 +7,4 @@ void _start();
 
 void arch_init(uint64_t cpuid){
     gic_init();
-    // if(cpuid == 0){
-    //     for(int i = 1; i < 4; i++){
-    //         psci_cpu_on(i, (uintptr_t) _start, 0);
-    //     }
-    // }
-    //asm volatile("MSR   DAIFClr, #2\n\t");
 }
--- src/arch/riscv/init.c
+++ src/arch/riscv/init.c
@@ -12,14 +12,6 @@ extern void _start();
 
 void arch_init(uint64_t cpu_id){
     hart_id = cpu_id;
-    // struct sbiret ret = (struct sbiret){ .error = SBI_SUCCESS };
-    // size_t i = 0;
-    // do {
-    //     if(i == hart_id) continue;
-    //     ret = sbi_hart_start(i, (unsigned long) &_start, 0);
-    // } while(i++, ret.error == SBI_SUCCESS);
     pt_init();
     plic_init();   
-    // CSRS(sie, SIE_SEIE);
-    // CSRS(sstatus, SSTATUS_SIE);
 }
