d548708cfad3b8a5d8eace33e80f255c9f746707
Jose Martins <josemartins90@gmail.com>
2022-12-11 17:12:43 +0000

    feat(armv8): add support for aarch32

---
+++ src/arch/armv8/aarch32/sources.mk
@@ -0,0 +1,2 @@
+sub_arch_c_srcs:= port.c
+sub_arch_s_srcs:= portASM.S freertos_vector_table.S
--- src/arch/armv8/init.c
+++ src/arch/armv8/init.c
@@ -1,6 +1,9 @@
 
 #include <gic.h>
+#include <sysregs.h>
 
 void arch_init(){
+    extern void _freertos_vector_table();
+    sysreg_vbar_el1_write(_freertos_vector_table);
     gic_init();
 }
