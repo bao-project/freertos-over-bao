a644d91a90c65ca1bc33e74596262baed4fecfca
Jose Martins <josemartins90@gmail.com>
2023-01-07 22:20:23 +0000

    fix: cast vector table pointer to integer to avoid warning

--- src/arch/armv8/init.c
+++ src/arch/armv8/init.c
@@ -4,6 +4,6 @@
 
 void arch_init(){
     extern void _freertos_vector_table();
-    sysreg_vbar_el1_write(_freertos_vector_table);
+    sysreg_vbar_el1_write((uintptr_t)_freertos_vector_table);
     gic_init();
 }
