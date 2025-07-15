Mechanicu/bao-freertos

update: freertos kernel version to v11.1.0
Signed-off-by: Jose Martins <josemartins90@gmail.com>

src/arch/riscv/inc/portContext.h
@@ -1,6 +1,6 @@
 /*
- * FreeRTOS Kernel V10.5.1
- * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * FreeRTOS Kernel V11.1.0
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * SPDX-License-Identifier: MIT
  *
@@ -30,13 +30,13 @@
 #define PORTCONTEXT_H
 
 #if __riscv_xlen == 64
-    #define portWORD_SIZE 8
-    #define store_x sd
-    #define load_x ld
+    #define portWORD_SIZE    8
+    #define store_x          sd
+    #define load_x           ld
 #elif __riscv_xlen == 32
-    #define store_x sw
-    #define load_x lw
-    #define portWORD_SIZE 4
+    #define store_x          sw
+    #define load_x           lw
+    #define portWORD_SIZE    4
 #else
     #error Assembler did not define __riscv_xlen
 #endif
@@ -49,37 +49,37 @@
  * specific version of freertos_risc_v_chip_specific_extensions.h.  See the
  * notes at the top of portASM.S file. */
 #ifdef __riscv_32e
-    #define portCONTEXT_SIZE ( 15 * portWORD_SIZE )
-    #define portCRITICAL_NESTING_OFFSET 13
-    #define portMSTATUS_OFFSET  14
+    #define portCONTEXT_SIZE               ( 15 * portWORD_SIZE )
+    #define portCRITICAL_NESTING_OFFSET    13
+    #define portMSTATUS_OFFSET             14
 #else
-    #define portCONTEXT_SIZE ( 31 * portWORD_SIZE )
-    #define portCRITICAL_NESTING_OFFSET 29
-    #define portMSTATUS_OFFSET  30
+    #define portCONTEXT_SIZE               ( 31 * portWORD_SIZE )
+    #define portCRITICAL_NESTING_OFFSET    29
+    #define portMSTATUS_OFFSET             30
 #endif
 
 /*-----------------------------------------------------------*/
 
 .extern pxCurrentTCB
-.extern xISRStackTop
-.extern xCriticalNesting
-.extern pxCriticalNesting
+   .extern xISRStackTop
+   .extern xCriticalNesting
+   .extern pxCriticalNesting
 /*-----------------------------------------------------------*/
 
-.macro portcontextSAVE_CONTEXT_INTERNAL
-    addi sp, sp, -portCONTEXT_SIZE
-    store_x x1, 1 * portWORD_SIZE( sp )
-    store_x x5, 2 * portWORD_SIZE( sp )
-    store_x x6, 3 * portWORD_SIZE( sp )
-    store_x x7, 4 * portWORD_SIZE( sp )
-    store_x x8, 5 * portWORD_SIZE( sp )
-    store_x x9, 6 * portWORD_SIZE( sp )
-    store_x x10, 7 * portWORD_SIZE( sp )
-    store_x x11, 8 * portWORD_SIZE( sp )
-    store_x x12, 9 * portWORD_SIZE( sp )
-    store_x x13, 10 * portWORD_SIZE( sp )
-    store_x x14, 11 * portWORD_SIZE( sp )
-    store_x x15, 12 * portWORD_SIZE( sp )
+   .macro portcontextSAVE_CONTEXT_INTERNAL
+addi sp, sp, -portCONTEXT_SIZE
+store_x x1, 1 * portWORD_SIZE( sp )
+store_x x5, 2 * portWORD_SIZE( sp )
+store_x x6, 3 * portWORD_SIZE( sp )
+store_x x7, 4 * portWORD_SIZE( sp )
+store_x x8, 5 * portWORD_SIZE( sp )
+store_x x9, 6 * portWORD_SIZE( sp )
+store_x x10, 7 * portWORD_SIZE( sp )
+store_x x11, 8 * portWORD_SIZE( sp )
+store_x x12, 9 * portWORD_SIZE( sp )
+store_x x13, 10 * portWORD_SIZE( sp )
+store_x x14, 11 * portWORD_SIZE( sp )
+store_x x15, 12 * portWORD_SIZE( sp )
 #ifndef __riscv_32e
     store_x x16, 13 * portWORD_SIZE( sp )
     store_x x17, 14 * portWORD_SIZE( sp )
@@ -97,96 +97,96 @@
     store_x x29, 26 * portWORD_SIZE( sp )
     store_x x30, 27 * portWORD_SIZE( sp )
     store_x x31, 28 * portWORD_SIZE( sp )
-#endif
+#endif /* ifndef __riscv_32e */
 
-    load_x  t0, xCriticalNesting         /* Load the value of xCriticalNesting into t0. */
-    store_x t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp ) /* Store the critical nesting value to the stack. */
-
-
-    csrr t0, sstatus                     /* Required for MPIE bit. */
-    store_x t0, portMSTATUS_OFFSET * portWORD_SIZE( sp )
+load_x t0, xCriticalNesting                                   /* Load the value of xCriticalNesting into t0. */
+store_x t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp ) /* Store the critical nesting value to the stack. */
 
 
-    portasmSAVE_ADDITIONAL_REGISTERS     /* Defined in freertos_risc_v_chip_specific_extensions.h to save any registers unique to the RISC-V implementation. */
+csrr t0, mstatus /* Required for MPIE bit. */
+store_x t0, portMSTATUS_OFFSET * portWORD_SIZE( sp )
 
-    load_x  t0, pxCurrentTCB             /* Load pxCurrentTCB. */
-    store_x  sp, 0( t0 )                 /* Write sp to first TCB member. */
 
-    .endm
+portasmSAVE_ADDITIONAL_REGISTERS /* Defined in freertos_risc_v_chip_specific_extensions.h to save any registers unique to the RISC-V implementation. */
+
+load_x t0, pxCurrentTCB          /* Load pxCurrentTCB. */
+store_x sp, 0 ( t0 )             /* Write sp to first TCB member. */
+
+   .endm
 /*-----------------------------------------------------------*/
 
-.macro portcontextSAVE_EXCEPTION_CONTEXT
-    portcontextSAVE_CONTEXT_INTERNAL
-    csrr a0, scause
-    csrr a1, sepc
-    addi a1, a1, 4                      /* Synchronous so update exception return address to the instruction after the instruction that generated the exception. */
-    store_x a1, 0( sp )                 /* Save updated exception return address. */
-    load_x sp, xISRStackTop             /* Switch to ISR stack. */
-    .endm
+   .macro portcontextSAVE_EXCEPTION_CONTEXT
+portcontextSAVE_CONTEXT_INTERNAL
+csrr a0, scause
+csrr a1, sepc
+addi a1, a1, 4          /* Synchronous so update exception return address to the instruction after the instruction that generated the exception. */
+store_x a1, 0 ( sp )    /* Save updated exception return address. */
+load_x sp, xISRStackTop /* Switch to ISR stack. */
+   .endm
 /*-----------------------------------------------------------*/
 
-.macro portcontextSAVE_INTERRUPT_CONTEXT
-    portcontextSAVE_CONTEXT_INTERNAL
-    csrr a0, scause
-    csrr a1, sepc
-    store_x a1, 0( sp )                 /* Asynchronous interrupt so save unmodified exception return address. */
-    load_x sp, xISRStackTop             /* Switch to ISR stack. */
-    .endm
+   .macro portcontextSAVE_INTERRUPT_CONTEXT
+portcontextSAVE_CONTEXT_INTERNAL
+csrr a0, scause
+csrr a1, sepc
+store_x a1, 0 ( sp )    /* Asynchronous interrupt so save unmodified exception return address. */
+load_x sp, xISRStackTop /* Switch to ISR stack. */
+   .endm
 /*-----------------------------------------------------------*/
 
-.macro portcontextRESTORE_CONTEXT
-    load_x  t1, pxCurrentTCB                /* Load pxCurrentTCB. */
-        load_x  sp, 0( t1 )                 /* Read sp from first TCB member. */
+   .macro portcontextRESTORE_CONTEXT
+load_x t1, pxCurrentTCB /* Load pxCurrentTCB. */
+load_x sp, 0 ( t1 )     /* Read sp from first TCB member. */
 
-    /* Load sepc with the address of the instruction in the task to run next. */
-    load_x t0, 0( sp )
-    csrw sepc, t0
+/* Load mepc with the address of the instruction in the task to run next. */
+load_x t0, 0 ( sp )
+csrw sepc, t0
 
-    /* Defined in freertos_risc_v_chip_specific_extensions.h to restore any registers unique to the RISC-V implementation. */
-    portasmRESTORE_ADDITIONAL_REGISTERS
+/* Defined in freertos_risc_v_chip_specific_extensions.h to restore any registers unique to the RISC-V implementation. */
+portasmRESTORE_ADDITIONAL_REGISTERS
 
-    /* Load mstatus with the interrupt enable bits used by the task. */
-    load_x  t0, portMSTATUS_OFFSET * portWORD_SIZE( sp )
-    csrw sstatus, t0                        /* Required for MPIE bit. */
+/* Load mstatus with the interrupt enable bits used by the task. */
+load_x t0, portMSTATUS_OFFSET * portWORD_SIZE( sp )
+csrw sstatus, t0                                             /* Required for MPIE bit. */
 
-    load_x  t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp )    /* Obtain xCriticalNesting value for this task from task's stack. */
-    load_x  t1, pxCriticalNesting           /* Load the address of xCriticalNesting into t1. */
-    store_x t0, 0( t1 )                     /* Restore the critical nesting value for this task. */
+load_x t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp ) /* Obtain xCriticalNesting value for this task from task's stack. */
+load_x t1, pxCriticalNesting                                 /* Load the address of xCriticalNesting into t1. */
+store_x t0, 0 ( t1 )                                         /* Restore the critical nesting value for this task. */
 
-    load_x  x1, 1 * portWORD_SIZE( sp )
-    load_x  x5, 2 * portWORD_SIZE( sp )
-    load_x  x6, 3 * portWORD_SIZE( sp )
-    load_x  x7, 4 * portWORD_SIZE( sp )
-    load_x  x8, 5 * portWORD_SIZE( sp )
-    load_x  x9, 6 * portWORD_SIZE( sp )
-    load_x  x10, 7 * portWORD_SIZE( sp )
-    load_x  x11, 8 * portWORD_SIZE( sp )
-    load_x  x12, 9 * portWORD_SIZE( sp )
-    load_x  x13, 10 * portWORD_SIZE( sp )
-    load_x  x14, 11 * portWORD_SIZE( sp )
-    load_x  x15, 12 * portWORD_SIZE( sp )
+load_x x1, 1 * portWORD_SIZE( sp )
+load_x x5, 2 * portWORD_SIZE( sp )
+load_x x6, 3 * portWORD_SIZE( sp )
+load_x x7, 4 * portWORD_SIZE( sp )
+load_x x8, 5 * portWORD_SIZE( sp )
+load_x x9, 6 * portWORD_SIZE( sp )
+load_x x10, 7 * portWORD_SIZE( sp )
+load_x x11, 8 * portWORD_SIZE( sp )
+load_x x12, 9 * portWORD_SIZE( sp )
+load_x x13, 10 * portWORD_SIZE( sp )
+load_x x14, 11 * portWORD_SIZE( sp )
+load_x x15, 12 * portWORD_SIZE( sp )
 #ifndef __riscv_32e
-    load_x  x16, 13 * portWORD_SIZE( sp )
-    load_x  x17, 14 * portWORD_SIZE( sp )
-    load_x  x18, 15 * portWORD_SIZE( sp )
-    load_x  x19, 16 * portWORD_SIZE( sp )
-    load_x  x20, 17 * portWORD_SIZE( sp )
-    load_x  x21, 18 * portWORD_SIZE( sp )
-    load_x  x22, 19 * portWORD_SIZE( sp )
-    load_x  x23, 20 * portWORD_SIZE( sp )
-    load_x  x24, 21 * portWORD_SIZE( sp )
-    load_x  x25, 22 * portWORD_SIZE( sp )
-    load_x  x26, 23 * portWORD_SIZE( sp )
-    load_x  x27, 24 * portWORD_SIZE( sp )
-    load_x  x28, 25 * portWORD_SIZE( sp )
-    load_x  x29, 26 * portWORD_SIZE( sp )
-    load_x  x30, 27 * portWORD_SIZE( sp )
-    load_x  x31, 28 * portWORD_SIZE( sp )
-#endif
-    addi sp, sp, portCONTEXT_SIZE
+    load_x x16, 13 * portWORD_SIZE( sp )
+    load_x x17, 14 * portWORD_SIZE( sp )
+    load_x x18, 15 * portWORD_SIZE( sp )
+    load_x x19, 16 * portWORD_SIZE( sp )
+    load_x x20, 17 * portWORD_SIZE( sp )
+    load_x x21, 18 * portWORD_SIZE( sp )
+    load_x x22, 19 * portWORD_SIZE( sp )
+    load_x x23, 20 * portWORD_SIZE( sp )
+    load_x x24, 21 * portWORD_SIZE( sp )
+    load_x x25, 22 * portWORD_SIZE( sp )
+    load_x x26, 23 * portWORD_SIZE( sp )
+    load_x x27, 24 * portWORD_SIZE( sp )
+    load_x x28, 25 * portWORD_SIZE( sp )
+    load_x x29, 26 * portWORD_SIZE( sp )
+    load_x x30, 27 * portWORD_SIZE( sp )
+    load_x x31, 28 * portWORD_SIZE( sp )
+#endif /* ifndef __riscv_32e */
+addi sp, sp, portCONTEXT_SIZE
 
-    sret
-    .endm
+mret
+   .endm
 /*-----------------------------------------------------------*/
 
 #endif /* PORTCONTEXT_H */

bao-project/freertos-over-bao

src/arch/riscv/inc/portContext.h
@@ -1,6 +1,6 @@
 /*
- * FreeRTOS Kernel V10.5.1
- * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * FreeRTOS Kernel V11.1.0
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * SPDX-License-Identifier: MIT
  *
@@ -30,13 +30,13 @@
 #define PORTCONTEXT_H
 
 #if __riscv_xlen == 64
-    #define portWORD_SIZE 8
-    #define store_x sd
-    #define load_x ld
+    #define portWORD_SIZE    8
+    #define store_x          sd
+    #define load_x           ld
 #elif __riscv_xlen == 32
-    #define store_x sw
-    #define load_x lw
-    #define portWORD_SIZE 4
+    #define store_x          sw
+    #define load_x           lw
+    #define portWORD_SIZE    4
 #else
     #error Assembler did not define __riscv_xlen
 #endif
@@ -49,37 +49,37 @@
  * specific version of freertos_risc_v_chip_specific_extensions.h.  See the
  * notes at the top of portASM.S file. */
 #ifdef __riscv_32e
-    #define portCONTEXT_SIZE ( 15 * portWORD_SIZE )
-    #define portCRITICAL_NESTING_OFFSET 13
-    #define portMSTATUS_OFFSET  14
+    #define portCONTEXT_SIZE               ( 15 * portWORD_SIZE )
+    #define portCRITICAL_NESTING_OFFSET    13
+    #define portMSTATUS_OFFSET             14
 #else
-    #define portCONTEXT_SIZE ( 31 * portWORD_SIZE )
-    #define portCRITICAL_NESTING_OFFSET 29
-    #define portMSTATUS_OFFSET  30
+    #define portCONTEXT_SIZE               ( 31 * portWORD_SIZE )
+    #define portCRITICAL_NESTING_OFFSET    29
+    #define portMSTATUS_OFFSET             30
 #endif
 
 /*-----------------------------------------------------------*/
 
 .extern pxCurrentTCB
-.extern xISRStackTop
-.extern xCriticalNesting
-.extern pxCriticalNesting
+   .extern xISRStackTop
+   .extern xCriticalNesting
+   .extern pxCriticalNesting
 /*-----------------------------------------------------------*/
 
-.macro portcontextSAVE_CONTEXT_INTERNAL
-    addi sp, sp, -portCONTEXT_SIZE
-    store_x x1, 1 * portWORD_SIZE( sp )
-    store_x x5, 2 * portWORD_SIZE( sp )
-    store_x x6, 3 * portWORD_SIZE( sp )
-    store_x x7, 4 * portWORD_SIZE( sp )
-    store_x x8, 5 * portWORD_SIZE( sp )
-    store_x x9, 6 * portWORD_SIZE( sp )
-    store_x x10, 7 * portWORD_SIZE( sp )
-    store_x x11, 8 * portWORD_SIZE( sp )
-    store_x x12, 9 * portWORD_SIZE( sp )
-    store_x x13, 10 * portWORD_SIZE( sp )
-    store_x x14, 11 * portWORD_SIZE( sp )
-    store_x x15, 12 * portWORD_SIZE( sp )
+   .macro portcontextSAVE_CONTEXT_INTERNAL
+addi sp, sp, -portCONTEXT_SIZE
+store_x x1, 1 * portWORD_SIZE( sp )
+store_x x5, 2 * portWORD_SIZE( sp )
+store_x x6, 3 * portWORD_SIZE( sp )
+store_x x7, 4 * portWORD_SIZE( sp )
+store_x x8, 5 * portWORD_SIZE( sp )
+store_x x9, 6 * portWORD_SIZE( sp )
+store_x x10, 7 * portWORD_SIZE( sp )
+store_x x11, 8 * portWORD_SIZE( sp )
+store_x x12, 9 * portWORD_SIZE( sp )
+store_x x13, 10 * portWORD_SIZE( sp )
+store_x x14, 11 * portWORD_SIZE( sp )
+store_x x15, 12 * portWORD_SIZE( sp )
 #ifndef __riscv_32e
     store_x x16, 13 * portWORD_SIZE( sp )
     store_x x17, 14 * portWORD_SIZE( sp )
@@ -97,96 +97,96 @@
     store_x x29, 26 * portWORD_SIZE( sp )
     store_x x30, 27 * portWORD_SIZE( sp )
     store_x x31, 28 * portWORD_SIZE( sp )
-#endif
+#endif /* ifndef __riscv_32e */
 
-    load_x  t0, xCriticalNesting         /* Load the value of xCriticalNesting into t0. */
-    store_x t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp ) /* Store the critical nesting value to the stack. */
-
-
-    csrr t0, sstatus                     /* Required for MPIE bit. */
-    store_x t0, portMSTATUS_OFFSET * portWORD_SIZE( sp )
+load_x t0, xCriticalNesting                                   /* Load the value of xCriticalNesting into t0. */
+store_x t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp ) /* Store the critical nesting value to the stack. */
 
 
-    portasmSAVE_ADDITIONAL_REGISTERS     /* Defined in freertos_risc_v_chip_specific_extensions.h to save any registers unique to the RISC-V implementation. */
+csrr t0, sstatus /* Required for MPIE bit. */
+store_x t0, portMSTATUS_OFFSET * portWORD_SIZE( sp )
 
-    load_x  t0, pxCurrentTCB             /* Load pxCurrentTCB. */
-    store_x  sp, 0( t0 )                 /* Write sp to first TCB member. */
 
-    .endm
+portasmSAVE_ADDITIONAL_REGISTERS /* Defined in freertos_risc_v_chip_specific_extensions.h to save any registers unique to the RISC-V implementation. */
+
+load_x t0, pxCurrentTCB          /* Load pxCurrentTCB. */
+store_x sp, 0 ( t0 )             /* Write sp to first TCB member. */
+
+   .endm
 /*-----------------------------------------------------------*/
 
-.macro portcontextSAVE_EXCEPTION_CONTEXT
-    portcontextSAVE_CONTEXT_INTERNAL
-    csrr a0, scause
-    csrr a1, sepc
-    addi a1, a1, 4                      /* Synchronous so update exception return address to the instruction after the instruction that generated the exception. */
-    store_x a1, 0( sp )                 /* Save updated exception return address. */
-    load_x sp, xISRStackTop             /* Switch to ISR stack. */
-    .endm
+   .macro portcontextSAVE_EXCEPTION_CONTEXT
+portcontextSAVE_CONTEXT_INTERNAL
+csrr a0, scause
+csrr a1, sepc
+addi a1, a1, 4          /* Synchronous so update exception return address to the instruction after the instruction that generated the exception. */
+store_x a1, 0 ( sp )    /* Save updated exception return address. */
+load_x sp, xISRStackTop /* Switch to ISR stack. */
+   .endm
 /*-----------------------------------------------------------*/
 
-.macro portcontextSAVE_INTERRUPT_CONTEXT
-    portcontextSAVE_CONTEXT_INTERNAL
-    csrr a0, scause
-    csrr a1, sepc
-    store_x a1, 0( sp )                 /* Asynchronous interrupt so save unmodified exception return address. */
-    load_x sp, xISRStackTop             /* Switch to ISR stack. */
-    .endm
+   .macro portcontextSAVE_INTERRUPT_CONTEXT
+portcontextSAVE_CONTEXT_INTERNAL
+csrr a0, scause
+csrr a1, sepc
+store_x a1, 0 ( sp )    /* Asynchronous interrupt so save unmodified exception return address. */
+load_x sp, xISRStackTop /* Switch to ISR stack. */
+   .endm
 /*-----------------------------------------------------------*/
 
-.macro portcontextRESTORE_CONTEXT
-    load_x  t1, pxCurrentTCB                /* Load pxCurrentTCB. */
-        load_x  sp, 0( t1 )                 /* Read sp from first TCB member. */
+   .macro portcontextRESTORE_CONTEXT
+load_x t1, pxCurrentTCB /* Load pxCurrentTCB. */
+load_x sp, 0 ( t1 )     /* Read sp from first TCB member. */
 
-    /* Load sepc with the address of the instruction in the task to run next. */
-    load_x t0, 0( sp )
-    csrw sepc, t0
+/* Load mepc with the address of the instruction in the task to run next. */
+load_x t0, 0 ( sp )
+csrw sepc, t0
 
-    /* Defined in freertos_risc_v_chip_specific_extensions.h to restore any registers unique to the RISC-V implementation. */
-    portasmRESTORE_ADDITIONAL_REGISTERS
+/* Defined in freertos_risc_v_chip_specific_extensions.h to restore any registers unique to the RISC-V implementation. */
+portasmRESTORE_ADDITIONAL_REGISTERS
 
-    /* Load mstatus with the interrupt enable bits used by the task. */
-    load_x  t0, portMSTATUS_OFFSET * portWORD_SIZE( sp )
-    csrw sstatus, t0                        /* Required for MPIE bit. */
+/* Load mstatus with the interrupt enable bits used by the task. */
+load_x t0, portMSTATUS_OFFSET * portWORD_SIZE( sp )
+csrw sstatus, t0                                             /* Required for MPIE bit. */
 
-    load_x  t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp )    /* Obtain xCriticalNesting value for this task from task's stack. */
-    load_x  t1, pxCriticalNesting           /* Load the address of xCriticalNesting into t1. */
-    store_x t0, 0( t1 )                     /* Restore the critical nesting value for this task. */
+load_x t0, portCRITICAL_NESTING_OFFSET * portWORD_SIZE( sp ) /* Obtain xCriticalNesting value for this task from task's stack. */
+load_x t1, pxCriticalNesting                                 /* Load the address of xCriticalNesting into t1. */
+store_x t0, 0 ( t1 )                                         /* Restore the critical nesting value for this task. */
 
-    load_x  x1, 1 * portWORD_SIZE( sp )
-    load_x  x5, 2 * portWORD_SIZE( sp )
-    load_x  x6, 3 * portWORD_SIZE( sp )
-    load_x  x7, 4 * portWORD_SIZE( sp )
-    load_x  x8, 5 * portWORD_SIZE( sp )
-    load_x  x9, 6 * portWORD_SIZE( sp )
-    load_x  x10, 7 * portWORD_SIZE( sp )
-    load_x  x11, 8 * portWORD_SIZE( sp )
-    load_x  x12, 9 * portWORD_SIZE( sp )
-    load_x  x13, 10 * portWORD_SIZE( sp )
-    load_x  x14, 11 * portWORD_SIZE( sp )
-    load_x  x15, 12 * portWORD_SIZE( sp )
+load_x x1, 1 * portWORD_SIZE( sp )
+load_x x5, 2 * portWORD_SIZE( sp )
+load_x x6, 3 * portWORD_SIZE( sp )
+load_x x7, 4 * portWORD_SIZE( sp )
+load_x x8, 5 * portWORD_SIZE( sp )
+load_x x9, 6 * portWORD_SIZE( sp )
+load_x x10, 7 * portWORD_SIZE( sp )
+load_x x11, 8 * portWORD_SIZE( sp )
+load_x x12, 9 * portWORD_SIZE( sp )
+load_x x13, 10 * portWORD_SIZE( sp )
+load_x x14, 11 * portWORD_SIZE( sp )
+load_x x15, 12 * portWORD_SIZE( sp )
 #ifndef __riscv_32e
-    load_x  x16, 13 * portWORD_SIZE( sp )
-    load_x  x17, 14 * portWORD_SIZE( sp )
-    load_x  x18, 15 * portWORD_SIZE( sp )
-    load_x  x19, 16 * portWORD_SIZE( sp )
-    load_x  x20, 17 * portWORD_SIZE( sp )
-    load_x  x21, 18 * portWORD_SIZE( sp )
-    load_x  x22, 19 * portWORD_SIZE( sp )
-    load_x  x23, 20 * portWORD_SIZE( sp )
-    load_x  x24, 21 * portWORD_SIZE( sp )
-    load_x  x25, 22 * portWORD_SIZE( sp )
-    load_x  x26, 23 * portWORD_SIZE( sp )
-    load_x  x27, 24 * portWORD_SIZE( sp )
-    load_x  x28, 25 * portWORD_SIZE( sp )
-    load_x  x29, 26 * portWORD_SIZE( sp )
-    load_x  x30, 27 * portWORD_SIZE( sp )
-    load_x  x31, 28 * portWORD_SIZE( sp )
-#endif
-    addi sp, sp, portCONTEXT_SIZE
+    load_x x16, 13 * portWORD_SIZE( sp )
+    load_x x17, 14 * portWORD_SIZE( sp )
+    load_x x18, 15 * portWORD_SIZE( sp )
+    load_x x19, 16 * portWORD_SIZE( sp )
+    load_x x20, 17 * portWORD_SIZE( sp )
+    load_x x21, 18 * portWORD_SIZE( sp )
+    load_x x22, 19 * portWORD_SIZE( sp )
+    load_x x23, 20 * portWORD_SIZE( sp )
+    load_x x24, 21 * portWORD_SIZE( sp )
+    load_x x25, 22 * portWORD_SIZE( sp )
+    load_x x26, 23 * portWORD_SIZE( sp )
+    load_x x27, 24 * portWORD_SIZE( sp )
+    load_x x28, 25 * portWORD_SIZE( sp )
+    load_x x29, 26 * portWORD_SIZE( sp )
+    load_x x30, 27 * portWORD_SIZE( sp )
+    load_x x31, 28 * portWORD_SIZE( sp )
+#endif /* ifndef __riscv_32e */
+addi sp, sp, portCONTEXT_SIZE
 
-    sret
-    .endm
+sret
+   .endm
 /*-----------------------------------------------------------*/
 
 #endif /* PORTCONTEXT_H */